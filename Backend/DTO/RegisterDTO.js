abfae9baa34e2dd2b2ea1da1deefdfeb44b149cc
AnnaPriscilla <annamoreira1985@gmail.com>
2026-02-16 08:00:22 -0300

    Feat: implementar registro de usuário com validação de email e criptografia de senha

---
+++ Backend/DTO/RegisterDTO.js
@@ -0,0 +1,11 @@
+package com.example.backend.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+public class RegisterDTO {
+    private String nome;
+    private String email;
+    private String password;
+}
